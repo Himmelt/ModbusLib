191d2cf63d33661ccf28d867579a24eeda27f4c5
Himmelt <master@void-3.cn>
2025-10-12 16:10:19 +0800

    test(ModbusDataConverter): 添加字节数组到数值数组的转换测试
    添加针对不同字节序和字序组合的测试用例，验证Convert方法对short、int、float和double数组的转换功能

--- ModbusLib.Tests/Models/ModbusDataConverterTests.cs
+++ ModbusLib.Tests/Models/ModbusDataConverterTests.cs
@@ -179,19 +179,100 @@ public class ModbusDataConverterTests {
 
     [Fact]
     public void Convert_Bytes_To_ShortArray() {
+        // 使用正向转换测试中的数据
+        var originalValues = new short[] { 0x1234, 0x5678 };
+        
+        // 测试大端字节序，高位优先
+        var bytes1 = new byte[] { 0x12, 0x34, 0x56, 0x78 };
+        var result11 = ModbusDataConverter.Convert<short>(bytes1, ByteOrder.BigEndian, WordOrder.HighFirst);
+        var result12 = ModbusDataConverter.Convert<short>(bytes1, ByteOrder.BigEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result11);
+        Assert.Equal(result11, result12);
 
+        // 测试小端字节序，高位优先
+        var bytes2 = new byte[] { 0x34, 0x12, 0x78, 0x56 };
+        var result21 = ModbusDataConverter.Convert<short>(bytes2, ByteOrder.LittleEndian, WordOrder.HighFirst);
+        var result22 = ModbusDataConverter.Convert<short>(bytes2, ByteOrder.LittleEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result21);
+        Assert.Equal(result21, result22);
     }
 
     [Fact]
     public void Convert_Bytes_To_IntArray() {
+        // 使用正向转换测试中的数据
+        var originalValues = new int[] { 0x12345678, 0x0abcdef0 };
+
+        // 测试大端字节序，高位优先
+        var bytes1 = new byte[] { 0x12, 0x34, 0x56, 0x78, 0x0a, 0xbc, 0xde, 0xf0 };
+        var result1 = ModbusDataConverter.Convert<int>(bytes1, ByteOrder.BigEndian, WordOrder.HighFirst);
+        Assert.Equal(originalValues, result1);
+
+        // 测试大端字节序，低位优先
+        var bytes2 = new byte[] { 0x56, 0x78, 0x12, 0x34, 0xde, 0xf0, 0x0a, 0xbc };
+        var result2 = ModbusDataConverter.Convert<int>(bytes2, ByteOrder.BigEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result2);
+
+        // 测试小端字节序，高位优先
+        var bytes3 = new byte[] { 0x34, 0x12, 0x78, 0x56, 0xbc, 0x0a, 0xf0, 0xde };
+        var result3 = ModbusDataConverter.Convert<int>(bytes3, ByteOrder.LittleEndian, WordOrder.HighFirst);
+        Assert.Equal(originalValues, result3);
+
+        // 测试小端字节序，低位优先
+        var bytes4 = new byte[] { 0x78, 0x56, 0x34, 0x12, 0xf0, 0xde, 0xbc, 0x0a };
+        var result4 = ModbusDataConverter.Convert<int>(bytes4, ByteOrder.LittleEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result4);
     }
 
     [Fact]
     public void Convert_Bytes_To_FloatArray() {
+        // 使用正向转换测试中的数据
+        var originalValues = new float[] { 123.45f, 678.90f };
+
+        // 测试大端字节序，高位优先
+        var bytes1 = new byte[] { 0x42, 0xF6, 0xE6, 0x66, 0x44, 0x29, 0xB9, 0x9A };
+        var result1 = ModbusDataConverter.Convert<float>(bytes1, ByteOrder.BigEndian, WordOrder.HighFirst);
+        Assert.Equal(originalValues, result1);
+
+        // 测试大端字节序，低位优先
+        var bytes2 = new byte[] { 0xE6, 0x66, 0x42, 0xF6, 0xB9, 0x9A, 0x44, 0x29 };
+        var result2 = ModbusDataConverter.Convert<float>(bytes2, ByteOrder.BigEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result2);
+
+        // 测试小端字节序，高位优先
+        var bytes3 = new byte[] { 0xF6, 0x42, 0x66, 0xE6, 0x29, 0x44, 0x9A, 0xB9 };
+        var result3 = ModbusDataConverter.Convert<float>(bytes3, ByteOrder.LittleEndian, WordOrder.HighFirst);
+        Assert.Equal(originalValues, result3);
+
+        // 测试小端字节序，低位优先
+        var bytes4 = new byte[] { 0x66, 0xE6, 0xF6, 0x42, 0x9A, 0xB9, 0x29, 0x44 };
+        var result4 = ModbusDataConverter.Convert<float>(bytes4, ByteOrder.LittleEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result4);
     }
 
     [Fact]
     public void Convert_Bytes_To_DoubleArray() {
+        // 使用正向转换测试中的数据
+        var originalValues = new double[] { 123.456789, 987.654321 };
+
+        // 测试大端字节序，高位优先
+        var bytes1 = new byte[] { 0x40, 0x5E, 0xDD, 0x3C, 0x07, 0xEE, 0x0B, 0x0B, 0x40, 0x8E, 0xDD, 0x3C, 0x0C, 0xA6, 0x00, 0xB0 };
+        var result1 = ModbusDataConverter.Convert<double>(bytes1, ByteOrder.BigEndian, WordOrder.HighFirst);
+        Assert.Equal(originalValues, result1);
+
+        // 测试大端字节序，低位优先
+        var bytes2 = new byte[] { 0x0B, 0x0B, 0x07, 0xEE, 0xDD, 0x3C, 0x40, 0x5E, 0x00, 0xB0, 0x0C, 0xA6, 0xDD, 0x3C, 0x40, 0x8E };
+        var result2 = ModbusDataConverter.Convert<double>(bytes2, ByteOrder.BigEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result2);
+
+        // 测试小端字节序，高位优先
+        var bytes3 = new byte[] { 0x5E, 0x40, 0x3C, 0xDD, 0xEE, 0x07, 0x0B, 0x0B, 0x8E, 0x40, 0x3C, 0xDD, 0xA6, 0x0C, 0xB0, 0x00 };
+        var result3 = ModbusDataConverter.Convert<double>(bytes3, ByteOrder.LittleEndian, WordOrder.HighFirst);
+        Assert.Equal(originalValues, result3);
+
+        // 测试小端字节序，低位优先
+        var bytes4 = new byte[] { 0x0B, 0x0B, 0xEE, 0x07, 0x3C, 0xDD, 0x5E, 0x40, 0xB0, 0x00, 0xA6, 0x0C, 0x3C, 0xDD, 0x8E, 0x40 };
+        var result4 = ModbusDataConverter.Convert<double>(bytes4, ByteOrder.LittleEndian, WordOrder.LowFirst);
+        Assert.Equal(originalValues, result4);
     }
 
     [Fact]
